1adcc4a93c2e98b91d1e4b99efef3df8e62502f0
Sandeep Sunny Nair <sunnynair54@yahoo.com>
2023-06-10 18:34:42 -0500

    more pseudo code added

--- App.java
+++ App.java
@@ -24,7 +24,7 @@ public class App {
         String urlString = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
         // retrieve the JSON data from the URL and return a list of "Item" objects.
         List<Item> itemList = fetchData(urlString);
-// check for "null" items 
+        // check for "null" items
         if (itemList != null) {
             // filter out "null" data
             itemList = filterItems(itemList);
@@ -41,21 +41,36 @@ public class App {
 
         try {
             URI uri = new URI(urlString);
+            // parse the URL data, but i needed to convert the URI to a URL
             URL url = uri.toURL();
+            // similar to Javascript I need to use a Get method to retrieve data from URL"
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // connect to the URL using this method
             connection.setRequestMethod("GET");
 
             int responseCode = connection.getResponseCode();
+            // status code of URL, if successful then continue with the input stream of data
             if (responseCode == HttpURLConnection.HTTP_OK) {
+                // this method is used to read the input stream from the URL
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                // Once we retrieve the input data, I can store it in an empty variable
                 StringBuilder response = new StringBuilder();
                 String line;
+                /*
+                 * Used a while loop to go through each line until there are no more lines of
+                 * data to go through
+                 * Then it exit the loop
+                 */
                 while ((line = reader.readLine()) != null) {
                     response.append(line);
                 }
                 reader.close();
 
                 JSONArray jsonArray = new JSONArray(response.toString());
+                /*
+                 * lets store all the data in an array and then loop through each
+                 * element of the array to get each items value
+                 */
                 for (int i = 0; i < jsonArray.length(); i++) {
                     JSONObject jsonObject = jsonArray.getJSONObject(i);
                     int id = jsonObject.getInt("id");
@@ -63,8 +78,10 @@ public class App {
                     String name = jsonObject.getString("name");
 
                     Item item = new Item(id, listId, name);
+                    // put it all in the item object we created
                     itemList.add(item);
                 }
+                // error in case the status is not okay
             } else {
                 System.out.println("Error: " + responseCode);
             }
